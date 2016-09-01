0e31b044f8e64c6e9928f02dddeb79a3c252019e
Mauricio <koitoer@gmail.com>
2016-09-01 17:36:18 -0600

    Word Count Map Reduce

--- hadoop_training/src/main/java/com/koitoer/sl/WordCount.java
+++ hadoop_training/src/main/java/com/koitoer/sl/WordCount.java
@@ -1,21 +1,53 @@
 package com.koitoer.sl;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 /**
+ * Run using $hadoop jar hadoop-training-1.0.jar com.koitoer.sl.WordCount inputPath outputPath
  * Created by mauricio.mena on 31/08/2016.
  */
 public class WordCount {
 
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration configuration = new Configuration();
+        Job job = new Job(configuration, "myWordCountJob");
+
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        //For the initial input of the mapper
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        //What will be the input type and what the output type
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1 );
+    }
+
     /**
      * Input is in the format of {offset, line} -- >  (key, value)
+     * For word count example the output will be (apple, 1) - (I, 1) - (banana, 1)
      */
     public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 
@@ -30,7 +62,18 @@ public class WordCount {
         }
     }
 
+    /**
+     * Input will be defined by (apple, 1,1,1,1,1) - (I, 1)
+     * Output will be (apple,5) - (I,1)
+     */
     public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable>{
 
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for(IntWritable x : values){
+                sum += x.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
     }
 }
